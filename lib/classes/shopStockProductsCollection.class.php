18ea3b11ee3cd9fe120aeec0c66248ab212a94ed
klxqz <klxqz@mail.ru>
2013-12-04 17:44:12 +0600

    next 04.12.2013 17:37

--- lib/classes/shopStockProductsCollection.class.php
+++ lib/classes/shopStockProductsCollection.class.php
@@ -1,29 +1,28 @@
 <?php
 
-class shopStockProductsCollection extends shopProductsCollection
-{
-    public function stockFilter()
-    {
+class shopStockProductsCollection extends shopProductsCollection {
+
+    public function stockFilter() {
         if ($this->filtered) {
             return;
         }
-        
+
         $stock_model = new shopStockPluginModel();
         $stocks = $stock_model->getActiveStocks();
-        
-        if(!$stocks) {
+
+        if (!$stocks) {
             return false;
         }
         $ids = array();
-        
-        foreach($stocks as $stock) {
+
+        foreach ($stocks as $stock) {
             $ids[] = $stock['product_id'];
         }
-        $this->where[] = 'p.id IN ('.  implode(',', $ids).')';
-        
-        
+        $this->where[] = 'p.id IN (' . implode(',', $ids) . ')';
+
+
         $this->filtered = true;
         return true;
     }
-    
+
 }
