4840f36ff853f39e9457a69d1cd8721981d0afd9
korobovn <korobovn@gmail>
2019-03-05 12:36:09 +0500

    v4.0.3

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'name' => 'Акции',
     'description' => 'Возможность организовать акцию на сайте',
     'vendor' => '985310',
-    'version' => '4.0.2',
+    'version' => '4.0.3',
     'img' => 'img/stock.png',
     'frontend' => true,
     'shop_settings' => true,
